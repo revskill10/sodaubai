a2bcca7b2a1ea05197a7dabf6fc452790ea2867a
Truong Hoang Dung <dungth@hpu.edu.vn>
2013-06-16 19:31:14 +0700

    add sign in spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
-
+require 'pp'
 describe "the signin process" do
   include RequestHelpers
  
-  	let(:authed_user) { create_logged_in_user }
-    before { login_as(authed_user, scope: :user) }
- 
-  it "should allow access" do
-    visit dashboard_index_path
-    page.should have_content("Welcome")
-    # should be good!
-  end
+	let(:authed_user) { create_logged_in_user }
+  before { login_as(authed_user, scope: :user) }
+   
   it "access lop mon hoc 1" do 
-  	visit '/lop_mon_hocs/1'
-  	page.should have_content("Xin chao")
+  	visit dashboard_index_path
+   
+  	page.should have_content("/lop_mon_hocs/1")
+
+  
   end
 end
