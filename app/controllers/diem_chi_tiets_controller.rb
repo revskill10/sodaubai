92a17d3dcf8104646a65ec48e6e866d191270c3c
Truong Hoang Dung <revskill09@gmail.com>
2013-07-15 16:29:01 +0700

    Add diem chi tiet

--- app/controllers/diem_chi_tiets_controller.rb
+++ app/controllers/diem_chi_tiets_controller.rb
@@ -4,7 +4,22 @@ class DiemChiTietsController < ApplicationController
     
     @svs = @lop_mon_hoc.get_sinh_viens
     respond_to do |format|
-      format.html # index.html.erb
+      format.html  do 
+        # index.html.erb
+        if @loai == 1 then 
+          if request.headers['X-PJAX']
+            render :index, :layout => false
+          else
+            render :index
+          end
+        else 
+          if request.headers['X-PJAX']
+            render :diemkiemtra, :layout => false
+          else
+            render :diemkiemtra
+          end
+        end
+      end
       format.json { head :no_content }
     end
   end
@@ -15,10 +30,38 @@ class DiemChiTietsController < ApplicationController
 
     respond_to do |format|
       if @diem_chi_tiet.save
-        format.html { redirect_to @diem_chi_tiet, notice: 'Diem chi tiet was successfully created.' }
+        format.html do 
+          if @loai == 1 then 
+          if request.headers['X-PJAX']
+            render :index, :layout => false
+          else
+            render :index
+          end
+        else 
+          if request.headers['X-PJAX']
+            render :diemkiemtra, :layout => false
+          else
+            render :diemkiemtra
+          end
+        end
+        end
         format.json { render json: @diem_chi_tiet, status: :created, location: @diem_chi_tiet }
       else
-        format.html { render action: "new" }
+        format.html do 
+          if @loai == 1 then 
+          if request.headers['X-PJAX']
+            render :index, :layout => false
+          else
+            render :index
+          end
+        else 
+          if request.headers['X-PJAX']
+            render :diemkiemtra, :layout => false
+          else
+            render :diemkiemtra
+          end
+        end
+        end
         format.json { render json: @diem_chi_tiet.errors, status: :unprocessable_entity }
       end
     end
@@ -29,8 +72,8 @@ class DiemChiTietsController < ApplicationController
     @lop_mon_hoc = LopMonHoc.find(params[:lop_mon_hoc_id])      
     @malop = @lop_mon_hoc.ma_lop
     @mamonhoc = @lop_mon_hoc.ma_mon_hoc
-    loai = params[:loai].to_i
-    @dct = @lop_mon_hoc.diem_chi_tiets.diem_thuc_hanh if loai==1
-    @dct = @lop_mon_hoc.diem_chi_tiets.diem_kiem_tra if loai==2
+    @loai = params[:loai].to_i
+    @dct = @lop_mon_hoc.diem_chi_tiets.diem_thuc_hanh if @loai==1
+    @dct = @lop_mon_hoc.diem_chi_tiets.diem_kiem_tra if @loai==2
   end
 end
--- app/controllers/sinh_viens_controller.rb
+++ app/controllers/sinh_viens_controller.rb
@@ -2,19 +2,12 @@ class SinhViensController < ApplicationController
   # GET /sinh_viens
   # GET /sinh_viens.json
   before_filter :authenticate_user!  
-  before_filter :load_lop  
-  def index        
-    authorize! :read, @lop_mon_hoc
-    @sinh_viens = @lop_mon_hoc.sinh_viens
+  #before_filter :load_lop  
+  def index            
+    @sinh_viens = SinhVien.all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @sinh_viens }
     end
-  end
-  
-  protected
-  def load_lop
-    @lop_mon_hoc = LopMonHoc.find(params[:lop_mon_hoc_id])
-  end
-    
+  end    
 end
