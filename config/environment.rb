dc0acdd7ac4c6da21a3030b0e76464a73a88d5cc
Truong Hoang Dung <revskill09@gmail.com>
2013-08-14 10:37:12 +0700

    Fix sinh vien bug

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 ENV['RAILS_ENV'] ||= 'production'
+
 #Rails.logger = Logger.new(STDOUT)
 # Initialize the rails application
 Trytest::Application.initialize!
