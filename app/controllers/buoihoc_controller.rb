e11b0da4b500ea50173769419e68256498f87c85
Truong Hoang Dung <revskill09@gmail.com>
2013-08-27 16:08:30 +0700

    Update quyen admin buoi hoc

--- app/controllers/buoihoc_controller.rb
+++ app/controllers/buoihoc_controller.rb
@@ -363,8 +363,9 @@ class BuoihocController < ApplicationController
       @ngayhoc = @type.get_days[:ngay] if @type.get_days
       @tkb = @type.get_tkbs.select {|k| k[:ma_lop] == @malop and k[:ma_mon_hoc] == @mamonhoc}.first
       @buoihoc = @ngayhoc.select {|l| to_zdate(l["time"][0]) == @ngay}[0] if @ngayhoc
-      @lich = @lop_mon_hoc.lich_trinh_giang_days.where(ngay_day: get_ngay(@ngay)).first_or_create!        
-    elsif current_user.is_admin?
+      @lich = @lop_mon_hoc.lich_trinh_giang_days.where(ngay_day: get_ngay(@ngay)).first_or_create! 
+    end       
+    if current_user.is_admin?
       @type = @lop_mon_hoc.giang_vien
       @ngayhoc = @type.get_days[:ngay]
       @tkb = @type.tkb_giang_viens.with_lop(@malop, @mamonhoc).first
