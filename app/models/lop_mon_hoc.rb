711f9d3fd3f061f74d8984432601c283b073a36d
Truong Hoang Dung <dungth@hpu.edu.vn>
2013-08-31 07:01:48 +0700

    Cap nhat danh sach phong

--- app/models/lop_mon_hoc.rb
+++ app/models/lop_mon_hoc.rb
@@ -33,6 +33,13 @@ class LopMonHoc < ActiveRecord::Base
   validates :ma_giang_vien, :ma_lop, :ma_mon_hoc, :presence => true
   validates_uniqueness_of :ma_lop, :scope => [:ma_giang_vien, :ma_mon_hoc]
   
+  PHONGS = {:A => [["A101","A102","A103"],["A201","A202","A204","A205"],["A301","A302","A303","A305","A306"],["A401","A402","A403","A404","A405"],["A501","A502"]],
+    :B => [["B101","B102","B103","B105"],["B201","B202","B203","B205"],["B301","B302","B303","B305"],["B401","B402","B403"]],
+    :C => [["C101","C102","C103","C104"],["C201","C202","C203","C204"],["C301","C302","C303","C304"]],
+    :D => [["D101","D102","D103"],["D201","D202","D203"],["D301","D302","D304"]],
+    :E => [["E301"]],
+    :F => [["F101B","F102"],["F201","F205"],["F302"]]}
+
   after_save :update_trogiang
   def to_s
     "#{ma_lop} #{ten_mon_hoc} #{ma_giang_vien}"
